b485be809a551d8fada48add0fa9a1b7f6eabc53
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2019-03-06 09:18:12 -0500

    fix 11

--- src/main/java/tutorial/core/banking/services/CoreService.java
+++ src/main/java/tutorial/core/banking/services/CoreService.java
@@ -170,7 +170,7 @@ public class CoreService {
 		
 		dataRepository.saveAccount(account);
 		
-		return TransferStatus.Fraud;
+		return TransferStatus.Valid;
 	}
 
 	public boolean IsThisAFraudTransfer(double amount, Account account) {
