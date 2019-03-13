e0959d79c2dd2cf318e127b2710eca66ac5bb9ff
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2019-03-13 15:49:57 -0400

    add descriptions

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,11 +1,5 @@
 import java.util.concurrent.TimeUnit;
 
-import tutorial.core.banking.consistency.ConsitencyChecker;
-import tutorial.core.banking.consistency.IChecker;
-import tutorial.core.banking.data.UserRepository;
-import tutorial.core.banking.infrastructure.ConsoleSender;
-import tutorial.core.banking.models.User;
-
 public class Startup {
 
 	public static void main(String[] args) throws InterruptedException {
--- src/main/java/tutorial/core/banking/models/InteracTransferStatus.java
+++
@@ -1,9 +0,0 @@
-package tutorial.core.banking.models;
-
-public enum InteracTransferStatus{
-	
-	Valid,
-	Fraud,
-	Error
-	
-}
